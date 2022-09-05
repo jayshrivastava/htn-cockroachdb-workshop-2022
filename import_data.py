bb4b16ac8df8fd48b1bb234b76763ead0e59b9ed
jayantsh <>
2022-09-05 19:37:36 +0000

    data: reduce airbnb data

--- import_data.py
+++ import_data.py
@@ -19,23 +19,34 @@ connection.set_session(autocommit=True)
 
 cursor = connection.cursor()
 
-
 # Columns
 # id,NAME,host id,host_identity_verified,host name,neighbourhood group,neighbourhood,
-# lat,long,country,country code,instant_bookable,cancellation_policy,room type,Construction year,price,service fee,minimum nights,number of reviews,last review,reviews per month,review rate number,calculated host listings count,availability 365,house_rules,license 
+# lat,long,country,country code,instant_bookable,cancellation_policy,room type,Construction year,price,service fee,minimum nights,number of reviews,last review,reviews per month,review rate number,calculated host listings count,availability 365,house_rules,license
 
 # Rows
 # 1006859,Cute & Cozy Lower East Side 1 bdrm,1280143094,verified,Miranda,Manhattan,Chinatown,
 # 40.71344,-73.99037,United States,US,FALSE,flexible,Entire home/apt,2004,
 # $319 ,$64 ,1,160,6/9/2019,1.33,3,4,1,,
 
-cursor.execute("CREATE TABLE airbnbs (id INT PRIMARY KEY, title STRING, neighbourhood_group STRING, neighbourhood STRING, host_name STRING, verified BOOL, year INT)")
+cursor.execute(
+    "CREATE TABLE airbnbs (id INT PRIMARY KEY, title STRING, neighbourhood_group STRING, neighbourhood STRING, host_name STRING, verified BOOL, year INT)"
+)
 
 with open("airbnbs.csv", "r") as f:
-  for line in f:
-    parts = line.strip().split(',')
-    # print(parts[0], parts[1], parts[5], parts[6], parts[4], parts[3] == "verified", parts[14])
-    cursor.execute("INSERT INTO airbnbs VALUES (%s, %s, %s, %s, %s, %s, %s)", parts[0], parts[1], parts[5], parts[6], parts[4], parts[3] == "verified", parts[14])
+    lines = f.readlines()
+  
+    # Print the column names
+    print(lines[0][0], lines[0][1], lines[0][5], lines[0][6], lines[0][4],
+              lines[0][3] == "verified", lines[0][14])
+  
+    for line in lines[1:]:
+        parts = line.strip().split(',')
+      
+        print(parts[0], parts[1], parts[5], parts[6], parts[4],
+              parts[3] == "verified", parts[14])
 
-print("Done.")
-
+        cursor.execute(
+            "INSERT INTO airbnbs VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (parts[0], parts[1], parts[5], parts[6], parts[4], parts[3]
+             == "verified", parts[14]))
+        connection.commit()
