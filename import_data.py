49bffd85fd4f2159d57669956d98c0ee51b4401b
Replit user <>
2022-09-15 16:03:29 +0000

    import: first line

--- import_data.py
+++ import_data.py
@@ -28,24 +28,25 @@ cursor = connection.cursor()
 # 40.71344,-73.99037,United States,US,FALSE,flexible,Entire home/apt,2004,
 # $319 ,$64 ,1,160,6/9/2019,1.33,3,4,1,,
 
-cursor.execute(
-    "CREATE TABLE airbnbs (id SERIAL PRIMARY KEY, title STRING, neighbourhood_group STRING, neighbourhood STRING, host_name STRING, verified BOOL, year INT)"
-)
+# cursor.execute(
+#     "CREATE TABLE airbnbs (id SERIAL PRIMARY KEY, title STRING, neighbourhood_group STRING, neighbourhood STRING, host_name STRING, verified BOOL, year INT)"
+# )
 
 with open("airbnbs.csv", "r") as f:
     lines = f.readlines()
   
     # Print the column names
-    print(lines[0][0], lines[0][1], lines[0][5], lines[0][6], lines[0][4],
-              lines[0][3] == "verified", lines[0][14])
+    first_line = lines[0].strip().split(',')
+    print(first_line[0], first_line[1], first_line[5], first_line[6], first_line[4],
+              first_line[3] == "verified", first_line[14])
   
-    for line in lines[1:]:
-        parts = line.strip().split(',')
+#     for line in lines[1:]:
+#         parts = line.strip().split(',')
       
-        print(parts[0], parts[1], parts[5], parts[6], parts[4],
-              parts[3] == "verified", parts[14])
+#         print(parts[0], parts[1], parts[5], parts[6], parts[4],
+#               parts[3] == "verified", parts[14])
 
-        cursor.execute(
-            "INSERT INTO airbnbs VALUES (%s, %s, %s, %s, %s, %s, %s)",
-            (parts[0], parts[1], parts[5], parts[6], parts[4], parts[3]
-             == "verified", parts[14]))
+#         cursor.execute(
+#             "INSERT INTO airbnbs VALUES (%s, %s, %s, %s, %s, %s, %s)",
+#             (parts[0], parts[1], parts[5], parts[6], parts[4], parts[3]
+#              == "verified", parts[14]))
